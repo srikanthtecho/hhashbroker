4be7a4d119ac3092f01a26b71618e90f44b07713
srikanth malyaala <srikanth malyaala>
2017-05-24 21:11:54 +0530

    first commit

---
+++ hashmap-service-broker/target/classes/import.sql
@@ -0,0 +1,2 @@
+insert into services (id, name, description, bindable) values ('1', 'HashBroker', 'Hash - HashMap as a Service', true)
+insert into plans (id, name, description, service_id) values ('1', 'basic', 'Basic Plan','1');
